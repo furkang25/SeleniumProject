8d18ee768b355e4074e2a635ff474053fd8437b2
Furkan Günes <97745699+furkang25@users.noreply.github.com>
2024-05-14 20:45:14 +0200

    v1.0

---
+++ Projekt1/src/de/codingsolo/selenium/tests/EdgeDriverSample.java
@@ -0,0 +1,22 @@
+package de.codingsolo.selenium.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class EdgeDriverSample {
+    public static void main(String[] args) throws Exception {
+        EdgeDriver driver = new EdgeDriver();
+        try {
+            driver.navigate().to("https://bing.com");
+
+            WebElement element = driver.findElement(By.id("sb_form_q"));
+            element.sendKeys("WebDriver");
+            element.submit();
+
+            Thread.sleep(5000);
+        } finally {
+            driver.quit();
+        }
+    }
+}
